024388ac3f61283cfdc9f52fc2cd3885be7fd5b0
Sem VB <144097969+semerded@users.noreply.github.com>
2023-11-12 14:27:17 +0100

    fix: fixed time units under 10 not showing a 0 in front

--- footer/footer.js
+++ footer/footer.js
@@ -5,19 +5,30 @@ fetch("/footer/footer.html")
   .then(data => {
     document.querySelector("footer").innerHTML = data;
   });
+function stableNumbers(unitToCheck) {
+    if (unitToCheck < 10) {
+    return '0' + unitToCheck;
+  };
+  return unitToCheck;
+}
 function timeInBelgium() {
   const dateTime = new Date();
-  const hours = dateTime.getHours();
-  const minutes = dateTime.getMinutes();
-  const seconds = dateTime.getSeconds();
-  const date = dateTime.getDate();
-  const month = dateTime.getMonth() + 1; // idk but the month had a weird kronkel in his head
+  let hours = dateTime.getHours();
+  hours = stableNumbers(hours);
+  let minutes = dateTime.getMinutes();
+  mintes = stableNumbers(mintutes);
+  let seconds = dateTime.getSeconds();
+  seconds = stableNumbers(seconds);
+  let date = dateTime.getDate();
+  date = stableNumbers(date);
+  let month = dateTime.getMonth() + 1; // idk but the month had a weird kronkel in his head
+  month = stableNumbers(month);
   const year = dateTime.getFullYear();
   console.log(dateTime)
   try {
     document.getElementById("time_in_belgium").innerHTML = `${date}-${month}-${year} ${hours}:${minutes}:${seconds}`;
   } catch (error) {
-    
+    // when the website isn't fully loaded yet it will cause an error
   }
 
   setTimeout(() => {
