d2fb6d139e08ce3d7d3a94cdc3fdac1bb11c8479
semerded <sem.van.broekhoven@gmail.com>
2023-11-16 09:52:55 +0100

    fix: corrections for the blog

--- subsites/internship/internship.js
+++ subsites/internship/internship.js
@@ -8,7 +8,7 @@ function rotateBoardImg() {
             document.getElementById("board_img").classList.add("board_img_spin_back");
             setTimeout(() => {
                 document.getElementById("board_img").classList.remove("board_img_spin_back");
-            }, 10); // prevents the image from spinning back
+            }, 30); // prevents the image from spinning back
             active = false;
         }, 1000);
     } 
